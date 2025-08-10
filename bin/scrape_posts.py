4f1dd2e871bfcd07dfbf56d0b66d60fba7d78f5d
hacksparr0w <hacksparr0w@protonmail.com>
2025-08-10 07:33:05 +0200

    chore: no longer use test registry dump destination

--- bin/scrape_posts.py
+++ bin/scrape_posts.py
@@ -130,7 +130,7 @@ async def main():
             registry.comments.update(comments)
 
         await client.stop()
-        await rmap.registry.dump(registry, _REGISTRY_DIRECTORY / "test")
+        await rmap.registry.dump(registry, _REGISTRY_DIRECTORY)
 
 
 if __name__ == "__main__":
